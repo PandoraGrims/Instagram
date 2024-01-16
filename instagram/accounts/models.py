dda3f94d799aae826f9ba5d968300c2407a20344
Pandora <nimfanefor@gmail.com>
2024-01-16 16:20:05 +0600

    Изменил модели, сделал миграции

--- instagram/accounts/models.py
+++ instagram/accounts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser, UserManager, Group, Permission
 from django.db import models
 
 
@@ -14,7 +14,7 @@ class CustomUserManager(UserManager):
         return self.filter(gender=self.GENDER_FEMALE)
 
 
-class User(AbstractUser):
+class Userkill(AbstractUser):
     avatar = models.ImageField(upload_to="avatars", verbose_name='Аватар')
     description = models.TextField(max_length=2000, verbose_name="Информация", blank=True, null=True)
     phone = models.CharField(max_length=30, verbose_name="Номер телефона", blank=True, null=True)
@@ -22,6 +22,18 @@ class User(AbstractUser):
     followers = models.ManyToManyField('self', related_name='following', verbose_name="Подписки", symmetrical=False)
 
     objects = CustomUserManager()
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=('groups'),
+        blank=True,
+        related_name='userkill_groups'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=('user permissions'),
+        blank=True,
+        related_name='userkill_user_permissions'
+    )
 
     def __str__(self):
         return self.user.username
